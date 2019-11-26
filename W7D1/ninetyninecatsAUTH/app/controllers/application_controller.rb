379a1bdec38e6e4fd624ab94b956d5210ff17fce
Anne Wong <hiuhin@gmail.com>
2019-11-25 17:56:23 -0800

    Add W7D1 classwork

---
+++ W7D1/ninetyninecatsAUTH/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user
+  
+  def current_user
+    return nil if session[:session_token].nil?
+
+    @user ||= User.find_by(session_token: session[:session_token])
+  end
+end
